7f474d5bf60242c56ac1928fafcea3562949c9c4
RafatMeraz <razhossain201@gmail.com>
2024-04-29 00:04:39 +0600

    Refactor cubit command code

--- cmd/cubit.go
+++ cmd/cubit.go
@@ -2,16 +2,13 @@ package cmd
 
 import (
 	"bb/code_templates"
+	"bb/utility"
 	"fmt"
-	"github.com/fatih/camelcase"
 	"github.com/spf13/cobra"
-	"log"
-	"os"
-	"strings"
 )
 
 var cubitCmd = &cobra.Command{
-	Use:   "cubit",
+	Use:   "cubit [name] [state]",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -20,92 +17,51 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		name, _ := cmd.Flags().GetString("name")
-		state, _ := cmd.Flags().GetString("state")
-		isPrimitive, _ := cmd.Flags().GetBool("p")
-		if name == "" {
-			name = "RandomCubit"
+		if len(args) == 0 {
+			cmd.Help()
+			return
 		}
-		if state == "" {
-			name = "int"
+		cubitName := args[0]
+
+		if len(args) >= 2 {
+			stateName := args[1]
+			createState(stateName, cubitName)
+			createCubitWithCustomState(cubitName, stateName)
+		} else {
+			primitiveType, _ := cmd.Flags().GetString("p")
+			if primitiveType == "" {
+				primitiveType = "int"
+			}
+			createCubitWithPrimitive(cubitName, primitiveType)
 		}
-		generateCubit(name, state, isPrimitive)
 	},
+}
+
+func createCubitWithPrimitive(cubitName string, primitiveType string) {
+	cubitFileName := utility.CreateFileName(cubitName)
+	file := utility.CreateFile(cubitFileName)
+	utility.WriteCodeOnFile(file, code_templates.CubitCode(cubitName, primitiveType))
+	message := fmt.Sprintf("%v is created", cubitName)
+	fmt.Println(message)
+}
+
+func createCubitWithCustomState(cubitName string, stateName string) {
+	cubitFileName := utility.CreateFileName(cubitName)
+	file := utility.CreateFile(cubitFileName)
+	stateFileName := utility.CreateFileName(stateName)
+	utility.WriteCodeOnFile(file, code_templates.CubitCodeWithCustomState(stateFileName, cubitName, stateName))
+	message := fmt.Sprintf("%v is created", cubitName)
+	fmt.Println(message)
+}
+
+func createState(stateName string, cubitName string) {
+	cubitFileName := utility.CreateFileName(cubitName)
+	cmd := &cobra.Command{}
+	cmd.Flags().String("partOf", cubitFileName, "")
+	stateCmd.Run(cmd, []string{stateName})
 }
 
 func init() {
 	generateCmd.AddCommand(cubitCmd)
-	cubitCmd.PersistentFlags().String("name", "", "Set name of the cubit")
-	cubitCmd.PersistentFlags().String("state", "", "Set name of the state")
-	cubitCmd.PersistentFlags().Bool("p", false, "Set name of the state")
-}
-
-func generateCubit(cubitName string, stateName string, isPrimitive bool) {
-	// generate cubit
-	cubitFileName := getCubitFileName(cubitName)
-	cubitFile := createFile(cubitFileName)
-	defer cubitFile.Close()
-
-	// generate state
-	if isPrimitive == false {
-		stateFileName := getCubitFileName(stateName)
-		stateFile := createFile(stateFileName)
-		defer stateFile.Close()
-		writeStateCode(stateName, cubitFileName, stateFile)
-
-		writeCubitCodeWithCustomState(stateFileName, cubitName, stateName, cubitFile)
-		fmt.Println(fmt.Sprintf("%v has been generated", cubitName))
-	} else {
-		writeCubitCode(cubitName, stateName, cubitFile)
-		fmt.Println(fmt.Sprintf("%v has been generated", cubitName))
-	}
-}
-
-func createFile(fileName string) *os.File {
-	file, err := os.Create(fileName)
-	if err != nil {
-		log.Fatal(err.Error())
-		os.Exit(1)
-	}
-	return file
-}
-
-func getCubitFileName(cubitName string) string {
-	name := getFileName(cubitName)
-	name = name + ".dart"
-	return name
-}
-
-func getFileName(cubitName string) string {
-	verbs := camelcase.Split(cubitName)
-	for i, v := range verbs {
-		verbs[i] = strings.ToLower(v)
-	}
-	fileName := strings.Join(verbs, "_")
-
-	return fileName
-}
-
-func writeCubitCode(cubitName string, stateName string, file *os.File) {
-	_, err := file.Write([]byte(code_templates.CubitCode(cubitName, stateName)))
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-}
-
-func writeCubitCodeWithCustomState(stateFileName string, cubitName string, stateName string, file *os.File) {
-	_, err := file.Write([]byte(code_templates.CubitCodeWithCustomState(stateFileName, cubitName, stateName)))
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-}
-
-func writeStateCode(stateName string, cubitFileName string, file *os.File) {
-	_, err := file.Write([]byte(code_templates.StateCode(stateName, cubitFileName)))
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
+	cubitCmd.PersistentFlags().String("p", "", "Set primitive type if state is dart primitive")
 }
